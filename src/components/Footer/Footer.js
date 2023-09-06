18b76deba758a2c5e52ae42f8633481572534593
Krristoff <bekrzysiek@gmail.com>
2023-09-07 00:17:14 +0200

    adding Footer files and basic styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+const Footer = () => {
+
+    return (
+        <footer className={styles.header}> 
+            Copyright©BandBlender     
+        </footer>
+    )
+}
+
+export default Footer;
