e27f8abe00d8d1edf01e8c9adfea76d8e162e367
KubaG1974 <kuba.gorzynski@gmail.com>
2023-09-21 15:29:19 +0200

    Installed axios and refactored api.js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,49 +1,41 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api'
+});
+
 export const loginUser = async (email, password) => {
-  const response = await fetch('http://localhost:5000/api/auth/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email, password })
-  });
-
-  if (!response.ok) {
-    throw new Error(`An error has occurred: ${response.status}`);
-  }
-
-  return await response.json();
+    try {
+        const response = await api.post('/auth/login', { email, password });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(`An error has occurred: ${error.response.status}`);
+        }
+        throw error;
+    }
 };
-
-
 
 export const registerUser = async (email, password) => {
-  const response = await fetch('http://localhost:5000/api/auth/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email, password })
-  });
-
-  if (!response.ok) {
-    throw new Error(`An error has occurred: ${response.status}`);
-  }
-
-  return await response.json();
+    try {
+        const response = await api.post('/auth/register', { email, password });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(`An error has occurred: ${error.response.status}`);
+        }
+        throw error;
+    }
 };
 
-
-
 export const initiatePasswordReset = async (email) => {
-  const response = await fetch('http://localhost:5000/api/auth/forgotpassword', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email })
-  });
-
-  if (!response.ok) {
-    throw new Error(`An error has occurred: ${response.status}`);
-  }
+    try {
+        const response = await api.post('/auth/forgotpassword', { email });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(`An error has occurred: ${error.response.status}`);
+        }
+        throw error;
+    }
 };
